1f6997e74143ce7653a5ed2426743a726fd89cb3
gerganajordanova <gerganajordanova68@gmail.com>
2018-03-12 17:40:04 +0200

    exercice 3 added!

---
+++ Lists exercice/3.Search_number/Search_number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int [] commands = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> newNums = new List<int>();
+            int numsToTake = commands[0];
+            int numsToDelete = commands[1];
+            int searchedNum = commands[2];
+            newNums.AddRange(nums.Take(numsToTake));
+            newNums.RemoveRange(0, commands[1]);
+            if (newNums.Contains(commands[2]))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+
+
+        }
+    }
+}
