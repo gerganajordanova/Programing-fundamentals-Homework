a52f4258e694499d13a59aeaed3e6d19ccbbc030
gerganajordanova <gerganajordanova68@gmail.com>
2018-03-14 15:22:10 +0200

    new exercice added!

---
+++ Lists exercice/4.Longest Increasing Subsequence (LIS)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Increasing_Subsequence__LIS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> sequence = new List<int>();
+            int [] len = new int[nums.Count];
+            int [] prev = new int[nums.Count];
+            int lenMax = 0;
+            int lastIndex = -1;
+            
+            for (int p = 0; p<nums.Count; p++)
+            {
+                len[p] = 1;
+                prev[p] = -1;
+
+                for (int left = 0; left<p; left++)
+                {
+                    if (nums[left] < nums[p] && len[left] + 1 > len[p])
+                    {
+                        len[p] = len[left] + 1;
+                        prev[p] = left;
+                    }
+                }
+
+                if (len[p] > lenMax)
+                {
+                    lenMax = len[p];
+                    lastIndex = p;
+                }
+            }
+            
+            while (lastIndex != -1)
+            {
+                sequence.Add(nums[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+            sequence.Reverse();
+            foreach (var num in sequence)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
