339eebe5c11cb21a3e3bd91bfa01a03058d59090
gerganajordanova <gerganajordanova68@gmail.com>
2018-03-15 12:39:05 +0200

    new exercice added!

---
+++ Lists-exercices/6.Sum Reversed Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.Sum_Reversed_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split().ToList();
+            int sumOfdigits = 0;
+
+            foreach (var num in numbers)
+            {
+               char [] digits= num.ToCharArray();
+
+                digits=digits.Reverse().ToArray();
+                int number = int.Parse(new string(digits));
+                sumOfdigits += number;
+            }
+            Console.WriteLine(sumOfdigits);
+        }
+    }
+}
