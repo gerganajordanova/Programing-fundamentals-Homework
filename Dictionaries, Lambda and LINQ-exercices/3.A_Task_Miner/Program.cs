86e53080bbac2adf5dbd77b5a2e11dfdf2d99cf0
gerganajordanova <gerganajordanova68@gmail.com>
2018-03-22 15:53:22 +0200

    new exercice added

---
+++ Dictionaries, Lambda and LINQ-exercices/3.A_Task_Miner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.A_Task_Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string command = Console.ReadLine();
+            int quantity = 0;
+            while (command != "stop")
+            {
+                quantity = int.Parse(Console.ReadLine());
+                if (resources.ContainsKey(command))
+                {
+                    resources[command] += quantity;
+                }
+                else
+                {
+                    resources.Add(command, quantity);
+                }
+                command = Console.ReadLine();
+            }
+            foreach(var material in resources)
+            {
+                Console.WriteLine($"{material.Key} -> {material.Value}");
+            }
+        }
+    }
+}
