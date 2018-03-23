227ead20aecbfdc9cf448553bd332c490f537ef8
gerganajordanova <gerganajordanova68@gmail.com>
2018-03-23 16:40:54 +0200

    new exercice added

---
+++ Dictionaries, Lambda and LINQ-exercices/5.Hand_of_Cards/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    private static int CalculatePoints(string card)
+    {
+        string power = card[0].ToString();
+        string type = card[card.Length - 1].ToString();
+        int powerAsNumber = 0;
+        int typeAsNumber = 0;
+        bool nPower = int.TryParse(power, out powerAsNumber);
+
+        if (power == "1")
+        {
+            power = "10";
+        }
+
+        if (nPower == false)
+        {
+            switch (power)
+            {
+                case "J":
+                    powerAsNumber = 11;
+                    break;
+                case "Q":
+                    powerAsNumber = 12;
+                    break;
+                case "K":
+                    powerAsNumber = 13;
+                    break;
+                case "A":
+                    powerAsNumber = 14;
+                    break;
+            }
+        }
+        else
+        {
+            switch (type)
+            {
+                case "S":
+                    typeAsNumber = 4;
+                    break;
+                case "H":
+                    typeAsNumber = 3;
+                    break;
+                case "D":
+                    typeAsNumber = 2;
+                    break;
+                case "C":
+                    typeAsNumber = 1;
+                    break;
+            }
+        }
+
+        int calcCardPoints = powerAsNumber * typeAsNumber;
+        return calcCardPoints;
+    }
+
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> scores = new Dictionary<string, int>();
+        Dictionary<string, string> people = new Dictionary<string, string>();
+        string[] input = Console.ReadLine().Split(':').ToArray();
+        string name = input[0];
+        string hand = input[1];
+        int sumOfHand = 0;
+
+        while (name != "JOKER")
+        {
+            if (people.ContainsKey(name))
+            {
+                people[name] += hand;
+            }
+            else
+            {
+                people.Add(name, hand);
+            }
+
+        }
+
+        foreach (var p in people)
+        {
+            List<string> playerCards = new List<string>();
+            playerCards = p.Value.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Distinct().ToList();
+
+            foreach (string card in playerCards)
+            {
+                sumOfHand += CalculatePoints(card);
+            }
+
+            if (scores.ContainsKey(p.Key))
+            {
+                scores[p.Key] += sumOfHand;
+            }
+            else
+            {
+                scores.Add(p.Key, sumOfHand);
+            }
+        }
+
+        foreach (var result in scores)
+        {
+            Console.WriteLine($"{result.Key}: {result.Value}");
+        }
+    }
+}
+
---
+++ Dictionaries, Lambda and LINQ-exercices/6.User_Logs/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, int>> users = new SortedDictionary<string, Dictionary<string, int>>();
+            string[] input = Console.ReadLine().Split().ToArray();
+            while(input[0]!="end")
+            {
+                string ip = input[0].Substring(3);
+                string user = input[2].Substring(5);
+                if (users.ContainsKey(user))
+                {
+                   if(users[user].ContainsKey(ip))
+                    {
+                        users[user][ip]+= 1;
+                    }
+                    else
+                    {
+                        users[user].Add(ip, 1);
+                    }
+                }
+                else
+                {
+                    users.Add(user, new Dictionary<string, int> { { ip, 1 } });
+                }
+                input = Console.ReadLine().Split().ToArray();
+            }
+            PrintResult(users);
+        }
+        private static void PrintResult(SortedDictionary<string, Dictionary<string, int>> users)
+        {
+           
+          foreach (KeyValuePair<string, Dictionary<string, int>> u in users)
+            {
+            Console.WriteLine($"{u.Key}: ");
+            int counter = 0;
+                foreach (var prop in u.Value)
+                {
+                    Console.Write($"{prop.Key} => {prop.Value}");
+                    if (counter < u.Value.Count - 1)
+                    {
+                        Console.Write(", ");
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine(".");
+                    }
+                }
+            }
+        }
+    }
+
