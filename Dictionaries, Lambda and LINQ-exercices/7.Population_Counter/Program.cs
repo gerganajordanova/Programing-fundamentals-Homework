0d2cedad1739d618c8d322ef66ab90ec26105602
gerganajordanova <gerganajordanova68@gmail.com>
2018-03-24 02:46:21 +0200

    new exercice added

---
+++ Dictionaries, Lambda and LINQ-exercices/7.Population_Counter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, Dictionary<string, double>> population = new Dictionary<string, Dictionary<string, double>>();
+        string[] input = Console.ReadLine().Split('|').ToArray();
+        while (input[0] != "report")
+        {
+            string country = input[1];
+            string city = input[0];
+            double pop = double.Parse(input[2]);
+
+            if (!population.ContainsKey(country))
+            {
+                population.Add(country, new Dictionary<string, double> { { city, pop } });
+            }
+
+            if (!population[country].ContainsKey(city))
+            {
+                population[country].Add(city, pop);
+            }
+
+            input = Console.ReadLine().Split('|').ToArray();
+        }
+
+        PrintResult(population);
+    }
+    private static void PrintResult(Dictionary<string, Dictionary<string, double>> population)
+    {
+        var OrderedCountries = population.OrderByDescending(p => p.Value.Values.Sum());
+        foreach (var pair in OrderedCountries)
+        {
+            var orderedCities = pair.Value.OrderByDescending(c => c.Value);
+
+            Console.WriteLine($"{pair.Key} (total population: {pair.Value.Sum(c => c.Value)})");
+            foreach (var city in orderedCities)
+            {
+                Console.WriteLine($"=>{city.Key}: {city.Value}");
+            }
+        }
+    }
+}
---
+++ Dictionaries, Lambda and LINQ-exercices/8.Logs_Aggregator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        
+        Dictionary<string, Dictionary<string, int>> logs = new Dictionary<string, Dictionary<string, int>>();
+        string[] input = Console.ReadLine().Split().ToArray();
+       for (int i=0; i<count; i++)
+        {
+            string ip = input[0];
+            string name = input[1];
+            int numbers = int.Parse(input[2]);
+            if (logs.ContainsKey(name))
+            {
+                if (logs[name].ContainsKey(ip))
+                {
+                    logs[name][ip] += numbers;
+                }
+                else
+                {
+                    logs[name].Add(ip, numbers);
+                }
+            }
+            else
+            {
+                logs.Add(name, new Dictionary<string, int> { { ip, numbers } });
+            }
+            input = Console.ReadLine().Split().ToArray();
+           
+        }
+        PrintResult(logs);
+    }
+    private static void PrintResult(Dictionary<string, Dictionary<string, int>> logs)
+    {
+
+         foreach (var user in logs.OrderBy(x => x.Key))
+        {
+            Console.Write($"{user.Key}: {user.Value.Values.Sum()} [");
+            int commaCounter = 0;
+            foreach (var data in user.Value.OrderBy(x => x.Key))
+            {
+                Console.Write($"{data.Key}");
+                if (commaCounter < user.Value.Count - 1)
+                {
+                    Console.Write(", ");
+                    commaCounter++;
+                }
+                else
+                {
+                    Console.WriteLine("]");
+                }
+            }
+        }
+
+    }
+}
