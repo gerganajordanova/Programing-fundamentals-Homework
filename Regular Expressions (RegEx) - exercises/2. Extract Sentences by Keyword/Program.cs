425a5cc16896e5aaf22bd56d6a4732c7ab41669d
gerganajordanova <gerganajordanova68@gmail.com>
2018-03-29 17:28:07 +0300

    Regex exercices added

---
+++ Regular Expressions (RegEx) - exercises/2. Extract Sentences by Keyword/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToMatch = Console.ReadLine().Trim();
+            string pattern = $@"(\b{wordToMatch}\b)";
+            string [] text=Console.ReadLine().Split(new[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string sentence in text)
+            {
+                Match match = Regex.Match(sentence, pattern);
+
+                if (match.Success)
+                {
+                    Console.WriteLine(sentence);
+                }
+
+            }
+        }
+    }
